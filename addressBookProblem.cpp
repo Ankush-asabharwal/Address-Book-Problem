b5a356a7400d16c6866fbdb277286567c6d59471
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2025-01-12 17:10:31 +0530

    [Ankusha] add: Adding the UC2

--- addressBookProblem.cpp
+++ addressBookProblem.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 
 using namespace std;
 
@@ -11,8 +12,9 @@ public :
     string state;
     int zip;
     long int phoneNumber;
+    string email;
 
-    Contacts(string firstName, string lastName, string address, string city, string state, int zip, long int phoneNumber){
+    Contacts(string firstName, string lastName, string address, string city, string state, int zip, long int phoneNumber, string email) {
         this->firstName = firstName;
         this->lastName = lastName;
         this->address = address;
@@ -20,10 +22,14 @@ public :
         this->state = state;
         this->zip = zip;
         this->phoneNumber = phoneNumber;
+        this->email = email;
+    }
+
+    Contacts() {
     }
 
 public:
-    void display(){
+    void display() {
         cout << "First Name : " << firstName << endl;
         cout << "Last Name : " << lastName << endl;
         cout << "Address : " << address << endl;
@@ -31,13 +37,50 @@ public:
         cout << "State : " << state << endl;
         cout << "Zip : " << zip << endl;
         cout << "Phone Number : " << phoneNumber << endl;    
+        cout << "Email : " << email << endl;
     }
 };
 
-int main(){
+class AddressBook {
+public :
+    list<Contacts> contacts;
+
+public:
+    void operator+ (Contacts &c){
+        contacts.push_back(c);
+    }
+
+    void display(){
+        for(auto contact : contacts){
+            contact.display();
+        }
+    }
+};
+
+int main() {
     cout << "Welcome to Book Address Problem" << endl;
 
-    Contacts c("Ankusha", "Sabharwal", "GTB nagar", "Ludhiana", "Punjab", 141010, 8360765920);
-    c.display();
-   
+    Contacts c;
+    cout << "Enter First Name: ";
+    cin >> c.firstName;
+    cout << "Enter Last Name: ";
+    cin >> c.lastName;
+    cout << "Enter Address : ";
+    cin >> c.address;
+    cout << "Enter City : ";
+    cin >> c.city;
+    cout << "Enter State : ";
+    cin >> c.state;
+    cout << "Enter ZIP Code: ";
+    cin >> c.zip;
+    cout << "Enter Phone Number: ";
+    cin >> c.phoneNumber;
+    cout << "Enter Email: ";
+    cin >> c.email;
+
+    cout << endl;
+
+    AddressBook ad;
+    ad + c;
+    ad.display();
 }
